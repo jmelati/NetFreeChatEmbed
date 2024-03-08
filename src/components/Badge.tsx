730dd0c449abd4b3eb6f22413c429f4e47c5d79b
Daniel <daniel.verloop@tappstr.com>
2024-03-08 11:49:03 +0100

    1e pogin branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -47,14 +47,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://jetbot.nl'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Jetbot</span>
       </a>
     </span>
   );
