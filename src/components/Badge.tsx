b9e262dc1c6b7aa1d2aa3b4aa9e4fdcc23c6bae3
jmelati <88042866+jmelati@users.noreply.github.com>
2024-06-09 07:39:49 -0300

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -47,14 +47,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://jetbot.nl'}
+        href={'https://www.netfree.com.br'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> JetBot</span>
+        <span> NetFree</span>
       </a>
     </span>
   );
